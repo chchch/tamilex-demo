ace1391558f1fd5a14f08815356a7d1b89869bfd
chchch <charles.li@cnrs.fr>
2023-02-24 10:07:46 +0100

    added query string

--- script.mjs
+++ script.mjs
@@ -1,7 +1,8 @@
-import {Transliterate} from 'https://tst-project.github.io/lib/js/transliterate.mjs';
-import {Sanscript} from 'https://tst-project.github.io/lib/js/sanscript.mjs';
+import { iastToTamil, tamilToIast } from './transliterate.mjs';
 import hljs from './highlight.min.js';
 import hlxml from './xml.min.js';
+import JSONCrush from './JSONCrush.min.js';
+
 hljs.registerLanguage('xml',hlxml);
 
 const state = {
@@ -16,6 +17,16 @@ const init = () => {
     state.wordin = document.getElementById('word_input');
     state.teiout = document.getElementById('tei_output');
     state.htmlout = document.getElementById('html_output');
+    const searchParams = new URLSearchParams(window.location.search);
+    if(searchParams.has('q')) {
+        const data = JSON.parse(
+            JSONCrush.uncrush(
+                decodeURIComponent(searchParams.get('q'))
+            )
+        );
+        state.textin.value = data.text || '';
+        state.wordin.value = data.words || '';
+    }
     for(const ta of [state.textin,state.wordin]) {
         ta.addEventListener('keyup',keyup);
         ta.addEventListener('blur',update);
@@ -27,10 +38,6 @@ const keyup = (e) => {
     if(e.keyCode === 13) update();
 };
 
-const tamilToIast = (text) => Sanscript.t(text,'tamil','iast')
-.replace(/^⁰|([^\d⁰])⁰/g,'$1¹⁰')
-.replace(/l̥/g,'ḷ');
-
 const update = async () => {
     const text = tamilToIast(state.textin.value);
     const words = tamilToIast(state.wordin.value);
@@ -41,6 +48,12 @@ const update = async () => {
     const out = await teitohtml(result);
     tamilize(out);
     state.htmlout.append(out);
+    const qs = encodeURIComponent(
+        JSONCrush.crush(
+            JSON.stringify({text: state.textin.value, words: state.wordin.value})
+        )
+    );
+    window.history.replaceState(null,null,window.location.pathname+`?q=${qs}`);
 };
 
 const collate = (text, words) => {
@@ -110,65 +123,6 @@ const tamilize = (frag) => {
 
     for(const rt of frag.querySelectorAll('rt'))
         rt.textContent = iastToTamil(rt.textContent,'iast','tamil');
-};
-const smush = (text,d_conv = false) => {
-    // d_conv is DHARMA convention
-    if(!d_conv) text = text.toLowerCase();
-
-    // remove space between word-final consonant and word-initial vowel
-    text = text.replace(/([gḍdrmvynhs]) ([aāiīuūṛeēoōêô])/g, '$1$2');
-
-    if(d_conv) text = text.toLowerCase();
-
-    // remove space between word-final consonant and word-intial consonant
-    text = text.replace(/([kgcjñḍtdnpbmrlyẏvśṣsṙ]) ([kgcjṭḍtdnpbmyẏrlvśṣshḻ])/g, '$1$2');
-
-    // join final o/e/ā and avagraha/anusvāra
-    text = text.replace(/([oōeēā]) ([ṃ'])/g,'$1$2');
-
-    text = text.replace(/ü/g,'\u200Cu');
-    text = text.replace(/ï/g,'\u200Ci');
-
-    text = text.replace(/_{1,2}(?=\s*)/g, function(match) {
-        if(match === '__') return '\u200D';
-        else if(match === '_') return '\u200C';
-    });
-
-    return text;
-};
-
-const iastToTamil = (text) => {
-    const txt = smush(text);
-    const grv = new Map([
-        ['\u0B82','\u{11300}'],
-        ['\u0BBE','\u{1133E}'],
-        ['\u0BBF','\u{1133F}'],
-        ['\u0BC0','\u{11340}'],
-        ['\u0BC1','\u{11341}'],
-        ['\u0BC2','\u{11342}'],
-        ['\u0BC6','\u{11347}'],
-        ['\u0BC7','\u{11347}'],
-        ['\u0BC8','\u{11348}'],
-        ['\u0BCA','\u{1134B}'],
-        ['\u0BCB','\u{1134B}'],
-        ['\u0BCC','\u{1134C}'],
-        ['\u0BCD','\u{1134D}'],
-        ['\u0BD7','\u{11357}']
-    ]);
-    const grc = ['\u{11316}','\u{11317}','\u{11318}','\u{1131B}','\u{1131D}','\u{11320}','\u{11321}','\u{11322}','\u{11325}','\u{11326}','\u{11327}','\u{1132B}','\u{1132C}','\u{1132D}'];
-
-    const smushed = text
-        .replace(/([kṅcñṭṇtnpmyrlvḻḷṟṉ])\s+([aāiīuūeēoō])/g, '$1$2')
-        .replace(/ḷ/g,'l̥')
-        .replace(/(^|\s)_ā/g,'$1\u0B85\u200D\u0BBE')
-        .replace(/(\S)([AĀIĪUŪEĒOŌ])/g,'$1\u200C$2')
-        .replace(/(\S)·/g,'$1\u200C')
-        .toLowerCase();
-    const rgex = new RegExp(`([${grc.join('')}])([${[...grv.keys()].join('')}])`,'g');
-    const pretext = Sanscript.t(smushed,'iast','tamil');
-    return pretext.replace(rgex, function(m,p1,p2) {
-        return p1+grv.get(p2); 
-    });
 };
 
 const cleanup = (doc) => {
---
+++ transliterate.mjs
@@ -0,0 +1,67 @@
+import {Sanscript} from 'https://tst-project.github.io/lib/js/sanscript.mjs';
+
+const smush = (text,d_conv = false) => {
+    // d_conv is DHARMA convention
+    if(!d_conv) text = text.toLowerCase();
+
+    // remove space between word-final consonant and word-initial vowel
+    text = text.replace(/([gḍdrmvynhs]) ([aāiīuūṛeēoōêô])/g, '$1$2');
+
+    if(d_conv) text = text.toLowerCase();
+
+    // remove space between word-final consonant and word-intial consonant
+    text = text.replace(/([kgcjñḍtdnpbmrlyẏvśṣsṙ]) ([kgcjṭḍtdnpbmyẏrlvśṣshḻ])/g, '$1$2');
+
+    // join final o/e/ā and avagraha/anusvāra
+    text = text.replace(/([oōeēā]) ([ṃ'])/g,'$1$2');
+
+    text = text.replace(/ü/g,'\u200Cu');
+    text = text.replace(/ï/g,'\u200Ci');
+
+    text = text.replace(/_{1,2}(?=\s*)/g, function(match) {
+        if(match === '__') return '\u200D';
+        else if(match === '_') return '\u200C';
+    });
+
+    return text;
+};
+
+const iastToTamil = (text) => {
+    const txt = smush(text);
+    const grv = new Map([
+        ['\u0B82','\u{11300}'],
+        ['\u0BBE','\u{1133E}'],
+        ['\u0BBF','\u{1133F}'],
+        ['\u0BC0','\u{11340}'],
+        ['\u0BC1','\u{11341}'],
+        ['\u0BC2','\u{11342}'],
+        ['\u0BC6','\u{11347}'],
+        ['\u0BC7','\u{11347}'],
+        ['\u0BC8','\u{11348}'],
+        ['\u0BCA','\u{1134B}'],
+        ['\u0BCB','\u{1134B}'],
+        ['\u0BCC','\u{1134C}'],
+        ['\u0BCD','\u{1134D}'],
+        ['\u0BD7','\u{11357}']
+    ]);
+    const grc = ['\u{11316}','\u{11317}','\u{11318}','\u{1131B}','\u{1131D}','\u{11320}','\u{11321}','\u{11322}','\u{11325}','\u{11326}','\u{11327}','\u{1132B}','\u{1132C}','\u{1132D}'];
+
+    const smushed = text
+        .replace(/([kṅcñṭṇtnpmyrlvḻḷṟṉ])\s+([aāiīuūeēoō])/g, '$1$2')
+        .replace(/ḷ/g,'l̥')
+        .replace(/(^|\s)_ā/g,'$1\u0B85\u200D\u0BBE')
+        .replace(/(\S)([AĀIĪUŪEĒOŌ])/g,'$1\u200C$2')
+        .replace(/(\S)·/g,'$1\u200C')
+        .toLowerCase();
+    const rgex = new RegExp(`([${grc.join('')}])([${[...grv.keys()].join('')}])`,'g');
+    const pretext = Sanscript.t(smushed,'iast','tamil');
+    return pretext.replace(rgex, function(m,p1,p2) {
+        return p1+grv.get(p2); 
+    });
+};
+
+const tamilToIast = (text) => Sanscript.t(text,'tamil','iast')
+.replace(/^⁰|([^\d⁰])⁰/g,'$1¹⁰')
+.replace(/l̥/g,'ḷ');
+
+export { iastToTamil, tamilToIast };
