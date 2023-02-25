9e4a2c12641ae917c946bdac2237f9a7c45211a6
chchch <charles.li@cnrs.fr>
2023-02-25 12:42:36 +0100

    fix lookup bug

--- script.mjs
+++ script.mjs
@@ -205,6 +205,7 @@ const toolTip = {
         tBoxDiv.myTarget = targ;
         tBox.appendChild(tBoxDiv);
         targ.addEventListener('mouseleave',toolTip.remove,{once: true});
+        targ.addEventListener('click',toolTip.remove,{once: true});
         //window.getComputedStyle(tBox).opacity;
         //tBox.style.opacity = 1;
         tBox.animate([
